f3f88bed3cc3564bbb39e96e91fcf139d353d472
Lucas <lucas.p@ges.inatel.br>
2020-06-17 20:42:41 -0300

    integrando card 1 e 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+    }
+    this.onSearchInputChange = this.onSearchInputChange.bind(this);
+  };
+
+  onSearchInputChange(event) {
+    this.setState({ searchInput: event.target.value })
+  }
+
   render() {
-    const { searchApi, searchText, onSearchTextChange } = this.props;
+    const { searchApi } = this.props;
     return (
-      <div>
+      <form onSubmit={(e) => { e.preventDefault(); searchApi(this.state.searchInput) }} className="form-inline mt-3">
         <input
           type="text"
           name="search"
+          className="form-control"
           size="50"
-          value={searchText}
-          onChange={onSearchTextChange}
+          value={this.state.searchInput}
+          onChange={this.onSearchInputChange}
         />
-        <button type="button" onClick={() => searchApi()}>Pesquisar</button>
-      </div>
+        <button type="button" className="btn btn-primary mx-1" onClick={() => searchApi(this.state.searchInput)}>Pesquisar</button>
+      </form>
     );
   }
 }
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,41 +1,57 @@
 import React from 'react';
-// import * as api from '../services/api';
+import * as api from '../services/api';
 import SearchBar from '../components/SearchBar';
+import CategoriesList from '../components/CategoriesList';
 
 class ProductList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchText: '',
-      // products: '',
+      selectedCategory: {},
+      products: [],
     };
-    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onSelectedCategoryChange = this.onSelectedCategoryChange.bind(this);
     this.searchApi = this.searchApi.bind(this);
   }
 
-  onSearchTextChange(event) {
-    this.setState({ searchText: event.target.value });
+  onSelectedCategoryChange(category) {
+    this.setState(state => ({ ...state, selectedCategory: category }));
+    api.getProductsFromCategory(category.id).then(data => this.setState(state => ({ ...state, products: data.results })))
   }
 
-  // searchApi() {
-  //   const { searchText } = this.state;
-  //   api.getProductsFromQuery(searchText).then((data) => this.setState({products: data.results}));
-  // }
+  searchApi(searchInput) {
+    this.setState(state => ({ ...state, searchText: searchInput }));
+
+    if (this.state.selectedCategory.id) {
+      api.getProductsFromCategoryAndQuery(this.state.selectedCategory.id, searchInput).then(data => this.setState(state => ({ ...state, products: data.results })))
+    } else {
+      api.getProductsFromQuery(searchInput).then(data => this.setState(state => ({ ...state, products: data.results })));
+    }
+  }
 
   render() {
-    const { searchText } = this.state;
+    const { searchText, selectedCategory } = this.state;
     // data não esta sendo usado ainda pois precisa criar o card dos produtos
     return (
-      <div>
-        <SearchBar
-          searchApi={this.searchApi}
-          searchText={searchText}
-          onSearchTextChange={this.onSearchTextChange}
-        />
-        <div>
-          <h4 data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </h4>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-3">
+            <CategoriesList
+              selectedCategory={selectedCategory}
+              onSelectCategory={this.onSelectedCategoryChange}
+            />
+          </div>
+          <div className="col-9">
+            <SearchBar
+              searchApi={this.searchApi}
+              searchText={searchText}
+              onSearchTextChange={this.onSearchTextChange}
+            />
+            <h4 data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </h4>
+          </div>
         </div>
       </div>
     );
