63a5cb3b7a6518879c6204036fc91ca37086d520
saturnian99 <saturnian.dsg@gmail.com>
2020-06-18 18:32:50 -0300

    Created functions to list cart items

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const CartItemCard = (props) => {
+  const { thumbnail, title, price } = props.product;
+  return (
+    <div>
+      <img src={thumbnail} height={100} alt="" />
+      <p>{title}</p>
+      <p>{price}</p>
+    </div>
+  );
+}
+
+const ListItems = (props) => {
+  const { products } = props;
+  return (
+    <section>
+      {products[0] === 'initial' && (
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      )}
+      {products[0] !== 'initial' && (
+        <div className="row">
+          {products.map(product => (
+            <CartItemCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </section>
+  );
+}
+
 class ShoppingCart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { products: '' };
+    this.state = { products: 'initial' };
   }
 
   render() {
+    const { products } = this.state;
 
     return (
       <div className="container-fluid">
         <div className="column">
           <Link to="/">Voltar</Link>
           <h4>Shopping Cart Page</h4>
-          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+
+          <ListItems products={products} />
         </div>
       </div>
     );
