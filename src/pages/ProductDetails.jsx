54ab37b0d64360243d1e3b505029416e0ee4c82d
Lucas <lucas.p@ges.inatel.br>
2020-06-22 20:36:52 -0300

    cc3

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FaTruck, FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa';
 import Avaliacoes from '../components/Avaliacoes';
 
 const CartIcon = (props) => {
